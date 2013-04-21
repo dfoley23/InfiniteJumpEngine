ccd528d7528a2694b997d8a48afb00c418cfa4fc
Kevin Cameron <kmcamero@ucsc.edu>
2013-04-20 20:45:40 -0700

    Re-sending the glIncludes header.

---
+++ InfiniteJumpEngine/InfiniteJumpEngine/glIncludes.h
@@ -0,0 +1,21 @@
+#ifndef GLINCLUDES_H
+#define GLINCLUDES_H
+
+#define GLEW_STATIC
+#include <GL/glew.h> //must include this before gl.h
+#include <GL/freeglut.h>
+
+#define GLM_SWIZZLE
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/matrix_access.hpp>
+
+#include <cmath>
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+#endif
