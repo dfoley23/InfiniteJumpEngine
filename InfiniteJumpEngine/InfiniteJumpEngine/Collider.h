a7e8aded15cd3db2f832776c352d893beef76124
Kevin Cameron <kmcamero@ucsc.edu>
2013-05-12 18:49:21 -0700

    Found a forumla for determining whether a point is within a triangle. This will help with tiles.

--- InfiniteJumpEngine/InfiniteJumpEngine/Collider.h
+++ InfiniteJumpEngine/InfiniteJumpEngine/Collider.h
@@ -2,7 +2,22 @@
 #include "Component.h"
 #include "Mesh.h"
 #include "glIncludes.h"
+/*
+ * References
+ * Point-in-triangle formula: http://www.blackpawn.com/texts/pointinpoly/
 
+ function SameSide(p1,p2, a,b)
+    cp1 = CrossProduct(b-a, p1-a)
+    cp2 = CrossProduct(b-a, p2-a)
+    if DotProduct(cp1, cp2) >= 0 then return true
+    else return false
+
+function PointInTriangle(p, a,b,c)
+    if SameSide(p,a, b,c) and SameSide(p,b, a,c)
+        and SameSide(p,c, a,b) then return true
+    else return false
+
+	*/
 class Collider : public Component
 {
 public:
