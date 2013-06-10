2d0c61895c90795f3b35c983ed10d5f6cc5f01d1
Kevin <kmcamero@ucsc.edu>
2013-06-09 18:10:20 -0700

    Starting to add profile support to the Lua script. This is a REQUIREMENT for the project.

--- InfiniteJumpEngine/InfiniteJumpEngine/MiniGolf.lua
+++ InfiniteJumpEngine/InfiniteJumpEngine/MiniGolf.lua
@@ -9,15 +9,19 @@ function loadDefaultScene()
 	--Define Course
 	local hole = 0
 	local filename = "Levels/course.db"
+	local profile = "Profiles/default.sav"
 	if argc > 1 then 
 		filename = argv[1]
 		if argc > 2 then
 			hole = argv[2]
+			if argc > 3 then
+				profile = argv[3]
+			end
 		end
 	end
 	print ("Filename:" .. filename)
 	print ("Hole:" .. hole)
-
+	print ("Profile:" .. profile)
 	--require(GolfHoleSelector)
 	--course = GolfHoleSelector.__init(filename, hole)
 	
@@ -28,11 +32,19 @@ function loadDefaultScene()
 	--hudRoot.addComponent(compass)
 	--hudRoot.addComponent(hudGLUI)
 	
+	--Define Profile
+	--open file
+	--read best scores into holes
+	
 	--root.addComponent(course)
 	--root.addComponent(hudRoot)
 	--root.branch = course
 	--root.shouldUpdate = function (comp) return (comp == root.branch) end
 	--root.shouldDraw = function (comp) return (comp == root.branch) end
+end
+
+function onExit()
+	--save best scores from each hole into profile
 end
 
 function setInitialCameraPos( )
